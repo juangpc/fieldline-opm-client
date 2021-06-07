38a55b43309cb8377af33ebb4328b2055fbea703
juangpc <juangpc@gmail.com>
2021-06-07 15:25:58 -0400

    add fieldlilne phantom class

--- api-example/mne_fieldline.py
+++ api-example/mne_fieldline.py
@@ -31,6 +31,44 @@ ft_IP = 'localhost'
 ft_port = 1972
 ft_data_type = FieldTrip.DATATYPE_FLOAT32
 
+
+class fieldline_phantom:
+
+    def __init__(self, num_sensors, sample_frequency):
+        self.num_sensors = num_sensors
+        self.sample_frequency = sample_frequency
+        self.num_samples = 200
+        self.ft_client = FieldTrip.Client()
+        self.send_data_flag = False
+        self.send_data_flag_lock = threading.Lock()
+        self.send_data_thread = threading.Thread(target=self.data_producer_routine, daemon = True)
+    
+    def __del__(self):
+        self.send_data_thread.join()
+
+    def connect(self, ip = 'localhost', port = 1972):
+        ft_IP = ip
+        ft_port = port
+        self.ft_client.connect(ft_IP, ft_port)
+        self.ft_client.putHeader(self.num_sensors, self.sample_frequency, FieldTrip.DATATYPE_FLOAT32)
+        self.send_data_thread.start()
+
+    def start(self):
+        self.send_data_flag_lock.acquire()
+        self.send_data_flag = True
+        self.send_data_flag_lock.release()
+
+    def stop(self):
+        self.send_data_flag_lock.acquire()
+        self.send_data_flag = False
+        self.send_data_flag_lock.release()
+
+    def data_producer_routine(self):
+        while True:
+            if self.send_data_flag:
+                data = numpy.random.rand(self.num_samples, self.num_sensors).astype(numpy.float32)
+                self.ft_client.putData(data)
+
 def num_working_sensors():
     num_sens = 0
     for ch in working_chassis:
